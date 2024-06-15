90cbbef13e82ee51b8f4501ede92d1c53a1d50df
Jose Antonio <jose.lima@fnde.gov.br>
2024-06-15 17:19:50 -0300

    add-mail-trap

---
+++ app/Mail/PaymentSuccessMail.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class PaymentSuccessMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $payment;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct($payment)
+    {
+        $this->payment = $payment;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->subject('Pagamento Processado')
+                    ->view('emails.payment');
+    }
+}
--- app/Services/PaymentService.php
+++ app/Services/PaymentService.php
@@ -2,10 +2,13 @@
 
 namespace App\Services;
 
+use App\Mail\PaymentFailMail;
+use App\Mail\PaymentSuccessMail;
 use App\Models\Payment;
 use App\Services\Contracts\PaymentServiceInterface;
 use App\Services\Payments\PaymentProcessorFactory;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Mail;
 
 class PaymentService implements PaymentServiceInterface
 {
@@ -15,10 +18,13 @@ class PaymentService implements PaymentServiceInterface
             $paymentMethod = $data['payment_method'];
             $processor = PaymentProcessorFactory::create($paymentMethod);
             $payment = Payment::create($data);
-            $processor->process($payment);            
+            $processor->process($payment);  
+            //aqui eu devo ter o email do payer      
+            Mail::to('recipient@example.com')->send(new PaymentSuccessMail($payment));    
             Log::info('Payment processed successfully: ' . $payment->id);
         } catch (\Exception $e) {
             Log::error('Error processing payment: ' . $e->getMessage());
+            Mail::to('recipient@example.com')->send(new PaymentFailMail($payment));  
         }
     }
 }
